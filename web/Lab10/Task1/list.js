f89f85e28609ff9bd8386f3635a44b3ae490f439
IcePhoenix09 <vlasuk.roma04@gmail.com>
2024-04-28 16:19:17 +0300

    lab10 task1 finished

--- web/Lab10/Task1/list.js
+++ web/Lab10/Task1/list.js
@@ -1,168 +1,184 @@
+// Method placeAtContext put items to html using appendChild. 
+// This method can be used to control items' position. 
+// context means place where items would be put using appendChild.
+
 class ActionButton{
-    constructor(context, label, onClick){
-        this.context = context;
-        this.onClick = onClick; 
-        this.label = label;
-        this.button = null;
-    }
-    placeAtContext(){
-        this.button = document.createElement('button');
-        this.button.innerText = this.label;
-        this.button.onclick = () => this.onClick();
-        this.context.appendChild(this.button);
-    }
-    remove(){
-        this.button.remove();
-    }
+  constructor(context, label, onClick){
+    this.context = context;
+    this.onClick = onClick; 
+    this.label = label;
+    this.button = null;
+  }
+  placeAtContext(){
+    this.button = document.createElement('button');
+    this.button.innerText = this.label;
+    this.button.onclick = () => this.onClick();
+    this.context.appendChild(this.button);
+  }
+  remove(){
+    this.button.remove();
+  }
 }
 
 class Line{
-    constructor(context, text){
-        this.context = context;
-        this.text = text
+  constructor(context, text, onDelete){
+    this.context = context;
+    this.text = text
+    this.onDelete = onDelete;
 
-        this.listItem = document.createElement('li');
-        this.br = document.createElement('br');
-        this.listItem.innerText = text;
+    // create elements
+    this.listItem = document.createElement('li');
+    this.br = document.createElement('br');
+    this.listItem.innerText = text;
 
-        //create button
-        this.deleteButton = new ActionButton(this.listItem, 'delete', () => {
-            this.deleteButton.remove();
-            this.remove();
-        });
-    }
+    //create button
+    this.deleteButton = new ActionButton(this.listItem, 'delete', () => {
+      this.remove();
+      onDelete();
+    });
+  }
 
-    placeAtContext(){
-        this.listItem.appendChild(document.createElement('br'));
-        this.deleteButton.placeAtContext();
-        this.context.appendChild(this.listItem);
-        this.context.appendChild(this.br);
-    }
+  placeAtContext(){
+    this.listItem.appendChild(document.createElement('br'));
+    this.deleteButton.placeAtContext();
+    this.context.appendChild(this.listItem);
+    this.context.appendChild(this.br);
+  }
 
-    remove(){
-        console.log(this.br);
-        this.listItem.remove();
-        this.br.remove();
-    }
+  remove(){
+    this.deleteButton.remove();
+    this.listItem.remove();
+    this.br.remove();
+  }
 }
 
 class SimpleList{
-    constructor(context, onChangeEvent){
-        this.context = context;
-        this.lines = [];
-        this.list = null;
-        this.onChangeEvent = onChangeEvent;
-    }
-    placeAtContext(){
-        this.list = document.createElement('ol');
-        this.context.appendChild(this.list);
-    }
+  constructor(context, onChangeEvent){
+    this.context = context;
+    this.lines = [];
+    this.list = null;
+    this.onChangeEvent = onChangeEvent;
+  }
 
-    add(item){
-        let line = new Line(this.list, item);
-        this.lines.push(line);
-        line.placeAtContext();
-    }
+  placeAtContext(){
+    this.list = document.createElement('ol');
+    this.context.appendChild(this.list);
+  }
 
-    addOneLine(item){
-        this.add(item);
-        document.dispatchEvent(this.onChangeEvent);
-    }
+  addOneLine(item){
+    let line = new Line(this.list, item, () => {
+      // update and save array when delete button is pressed on line 
+      let index = this.lines.indexOf(line);
+      this.lines.splice(index, 1);
+      this.saveChanges();
+    });
 
-    addMultipleLines(items){
-        for (let item of items){
-            this.add(item);
-        }
-        document.dispatchEvent(this.onChangeEvent);
-    }
-    
-    deleteAllLines(){
-        for (let i in this.lines){
-            this.lines[i].remove(); // remove item from html
-        }
-        this.lines.length = 0; // crear array
-        document.dispatchEvent(this.onChangeEvent);
-    }
+    this.lines.push(line);
+    line.placeAtContext();
+  }
 
-    deleteLine(string){
-        let line = this.lines.find((line) => line.text === string);
-        line.remove(); // remove item from html
-        this.lines.splice(this.lines.indexOf(line), 1); // remove item from array
-        document.dispatchEvent(this.onChangeEvent);
-    };
-
-    replaceLines(items){
-        this.deleteAllLines();
-        this.addMultipleLines(items);
+  addMultipleLines(items){
+    for (let item of items){
+      this.addOneLine(item);
     }
+  }
+  
+  deleteAllLines(){
+    for (let i in this.lines){
+      this.lines[i].remove(); // remove item from html
+    }
+    this.lines.length = 0; // clear array
+  }
+
+  deleteLine(string){
+    let line = this.lines.find((line) => line.text === string);
+    line.remove(); // remove item from html
+    this.lines.splice(this.lines.indexOf(line), 1); // remove item from array
+    this.saveChanges();
+  };
+
+  replaceLines(items){
+    this.deleteAllLines();
+    this.addMultipleLines(items);
+    this.saveChanges();
+  }
+
+  saveChanges(){
+    console.log("event")
+    document.dispatchEvent(this.onChangeEvent);
+  }
 }
 
+// simple input form that contains one input field and button
 class SimpleForm{
-    constructor(context, onSubmit){
-        this.context = context;
-        this.onSubmit = onSubmit;
-        
-        this.form = document.createElement('form');
-    };
-    placeAtContext(){
-        this.context.appendChild(this.form);
+  constructor(context, onSubmit){
+    this.context = context;
+    this.onSubmit = onSubmit;
+    
+    this.form = document.createElement('form');
+  };
+  placeAtContext(){
+    this.context.appendChild(this.form);
 
-        const input = document.createElement("input");
-        input.placeholder = "Name"
-        this.form.appendChild(input);
+    const input = document.createElement("input");
+    input.placeholder = "Name"
+    this.form.appendChild(input);
 
-        const button = document.createElement('button');
-        button.innerText = 'Submit' 
-        this.form.appendChild(button);
+    const button = document.createElement('button');
+    button.innerText = 'Submit' 
+    this.form.appendChild(button);
 
-        this.form.onsubmit = (event) => {
-            event.preventDefault();
+    this.form.onsubmit = (event) => {
+      event.preventDefault();
 
-            const value = input.value;
-            this.onSubmit(value);
-        }
-    };
-
-    remove(){
-        this.form.remove();
+      const value = input.value;
+      this.onSubmit(value);
     }
+  };
+
+  remove(){
+    this.form.remove();
+  }
 }
 
+// contains of two buttons: add(new item to the list) and delete(all items)
 class Panel{
-    constructor(contex, list){
-        this.contex = contex;
-        
-        this.addButtonField = document.createElement('div');
-        this.addButton = new ActionButton(this.addButtonField, 'Add new', () =>{
-            this.createInputField();
-        })
-        this.addButton.placeAtContext();
+  constructor(contex, list){
+    this.contex = contex;
+    
+    this.addButtonField = document.createElement('div');
+    this.addButton = new ActionButton(this.addButtonField, 'Add new', () =>{
+      this.createInputField();
+    })
+    this.addButton.placeAtContext();
 
-        this.deleteButtonField = document.createElement('div');
-        this.deleteButton = new ActionButton(this.deleteButtonField, 'Delete All', () => {
-            list.deleteAllLines();
-        });
-        this.deleteButton.placeAtContext();
-    }
+    this.deleteButtonField = document.createElement('div');
+    this.deleteButton = new ActionButton(this.deleteButtonField, 'Delete All', () => {
+      list.deleteAllLines();
+      list.saveChanges();
+    });
+    this.deleteButton.placeAtContext();
+  }
 
-    placeAtContex(){
-        console.log()
-        this.contex.appendChild(this.addButtonField);
-        this.contex.appendChild(document.createElement('br'));
-        this.contex.appendChild(this.deleteButtonField);
-    }
+  placeAtContex(){
+    console.log()
+    this.contex.appendChild(this.addButtonField);
+    this.contex.appendChild(document.createElement('br'));
+    this.contex.appendChild(this.deleteButtonField);
+  }
 
-    createAddButton(){
-        this.addButton.placeAtContext();
-    }
+  createAddButton(){
+    this.addButton.placeAtContext();
+  }
 
-    createInputField(){
-        this.addButton.remove();
-        let form = new SimpleForm(this.addButtonField, (inputValue) => {
-            list.addOneLine(inputValue);
-            form.remove();
-            this.createAddButton();
-        })
-        form.placeAtContext();
-    }
+  createInputField(){
+    this.addButton.remove();
+    let form = new SimpleForm(this.addButtonField, (inputValue) => {
+      list.addOneLine(inputValue);
+      form.remove();
+      this.createAddButton();
+      list.saveChanges();
+    })
+    form.placeAtContext();
+  }
 }
--- web/Lab10/Task1/main.js
+++ web/Lab10/Task1/main.js
@@ -1,16 +1,32 @@
-let list = new SimpleList(document.body);
+// this event is called to save data
+const onChangeEvent = new Event("listChange")
+
+let list = new SimpleList(document.body, onChangeEvent);
 let panel = new Panel(document.body, list);
 
+// display items on html
 list.placeAtContext();
-list.addMultipleLines(['test', 'test1']);
-
 panel.placeAtContex();
 
-const onChangeEvent = new Event("listChange");
-
-function saveList(list){
-    for (line of list.lines){
-        console.log(line.text);
-    }
+function saveList(){
+  items = [];
+  for (line of list.lines){
+    items.push(line.text);
+  }
+  localStorage.setItem("list", JSON.stringify(items));
 }
-saveList(list);
+
+// get data from storage. If data is not found list is filled with test data
+try{
+  items = JSON.parse(localStorage.getItem("list"));
+  list.addMultipleLines(items);
+} catch (error) {
+  console.warn('[Warning] saved data not found:', error);
+  list.addMultipleLines(['test data', 'test data 2']);
+  saveList(list);
+};
+
+document.addEventListener("listChange", e => {
+  console.log("Saving Data...")
+  saveList();
+})
